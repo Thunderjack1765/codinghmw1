5d9aec0d3a0ba66ffb3dbb48763f5e3fa330120a
Thunderjack1765 <Jbenson1765@gmail.com>
2022-08-16 15:21:38 -0500

    homework

---
+++ index.js
@@ -0,0 +1,17 @@
+let itemPrice = 10
+let moneyInWallet = 50
+let numberOfFriends = 5
+let age = 27
+let firstName = `Jordan`
+let lastName = `Benson`
+let middleInitial = `C`
+
+moneyInWallet = moneyInWallet - itemPrice
+console.log(`Money left in wallet ` + moneyInWallet);
+
+
+numberOfFriends = age/numberOfFriends
+console.log(`I made ` + numberOfFriends + ` friends throughout my life`);
+
+
+console.log(`My full name is ` + firstName + middleInitial + lastName);
